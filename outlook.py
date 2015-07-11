10af29654c66f62cb5ab292dc26da70a3b72e913
Rolly Maulana Awangga <rolly@awang.ga>
2015-07-11 22:49:35 +0700

    add read index

--- outlook.py
+++ outlook.py
@@ -1,9 +1,12 @@
 import imaplib
 import email
 import smtplib
+import datetime
 
 class Outlook():
 	def __init__(self):
+		mydate = datetime.datetime.now()
+		self.today = mydate.strftime("%d-%b-%Y")
 		self.imap = imaplib.IMAP4_SSL('imap-mail.outlook.com')
 		self.smtp = smtplib.SMTP('smtp-mail.outlook.com')
 		
@@ -59,27 +62,73 @@ class Outlook():
 	def logout(self):
 		return self.imap.logout()
 		
-	def unread(self):
+	def today(self):
+		mydate = datetime.datetime.now()
+		return mydate.strftime("%d-%b-%Y")
+		
+	def unreadIdsToday(self):
+		r, d = self.imap.search(None,'(SINCE "'+self.today+'")', 'UNSEEN')
+		list = d[0].split(' ')
+		return list
+		
+	def getIdswithWord(self,ids,word):
+		stack = []
+		for id in ids:
+			self.getEmail(id)
+			curr_mailmsg = self.mailbody()
+			if word in self.mailbody().lower():
+				stack.append(id)
+		return stack
+		
+	def unreadIds(self):
 		r, d = self.imap.search(None, "UNSEEN")
 		list = d[0].split(' ')
-		latest_id = list[-1]
-		r, d = self.imap.fetch(latest_id, "(RFC822)")
-		self.raw_email = d[0][1]
-		self.email_message = email.message_from_string(self.raw_email)
-		return self.email_message
-	
-	def read(self):
+		return list
+		
+	def readIdsToday(self):
+		r, d = self.imap.search(None,'(SINCE "'+self.today+'")', 'SEEN')
+		list = d[0].split(' ')
+		return list
+		
+	def readIds(self):
 		r, d = self.imap.search(None, "SEEN")
 		list = d[0].split(' ')
-		latest_id = list[-1]
-		r, d = self.imap.fetch(latest_id, "(RFC822)")
+		return list
+		
+	def getEmail(self,id):
+		r, d = self.imap.fetch(id, "(RFC822)")
 		self.raw_email = d[0][1]
 		self.email_message = email.message_from_string(self.raw_email)
 		return self.email_message
 		
+	def unread(self):
+		list = self.unreadIds()
+		latest_id = list[-1]
+		return self.getEmail(latest_id)
+	
+	def read(self):
+		list = self.readIds()
+		latest_id = list[-1]
+		return self.getEmail(latest_id)
+		
+	def readToday(self):
+		list = self.readIdsToday()
+		latest_id = list[-1]
+		return self.getEmail(latest_id)
+	
+	def unreadToday(self):
+		list = self.unreadIdsToday()
+		latest_id = list[-1]
+		return self.getEmail(latest_id)
+		
+	def readOnly(self,folder):
+		return self.imap.select(folder,readonly=True)
+	
+	def writeEnable(self,folder):
+		return self.imap.select(folder,readonly=False)
+				
 	def rawRead(self):
-		r, d = self.imap.search(None, "SEEN")
-		list = d[0].split(' ')
+		list = self.readIds()
 		latest_id = list[-1]
 		r, d = self.imap.fetch(latest_id, "(RFC822)")
 		self.raw_email = d[0][1]
@@ -102,5 +151,4 @@ class Outlook():
 		return self.email_message['from']
 		
 	def mailto(self):
-		return self.email_message['to']
-		
+		return self.email_message['to']	
